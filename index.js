7f3e9a95275d017460d8c61dd75f2560bf8485df
josedav9 <josedav_96@hotmail.com>
2020-08-19 20:01:46 -0500

    [UPD]: better format

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app =  express()
+const app = express();
 
-
-app.listen(3000, ()=>{
-  console.log("Servidor escuchando por el puerto 3000")
-})
+app.listen(3000, () => {
+  console.log("Servidor escuchando por el puerto 3000");
+});

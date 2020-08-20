14aae9027e51e4a432016b457a56b05ae1bddd20
josedav9 <josedav_96@hotmail.com>
2020-08-19 20:03:10 -0500

    [ADD]: adding start script

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json())
+
 app.listen(3000, () => {
   console.log("Servidor escuchando por el puerto 3000");
 });

53ce13d35c2d9bb1564ac5d7ae517df4961a5099
Jose Alvarez <jose.alvarez@lever.co>
2020-08-19 19:46:23 -0500

    [ADD]: scafold express homebank

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app =  express()
+
+
+app.listen(3000, ()=>{
+  console.log("Servidor escuchando por el puerto 3000")
+})
